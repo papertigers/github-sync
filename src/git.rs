08ba97cb791c42459c2be2131427d9794735c1e7
Mike Zeller <mike@mikezeller.net>
2021-02-24 22:05:41 -0500

    use git clone --mirror

--- src/git.rs
+++ src/git.rs
@@ -8,7 +8,7 @@ fn do_fetch<'a>(
     remote: &'a mut git2::Remote,
 ) -> Result<Option<git2::AnnotatedCommit<'a>>, git2::Error> {
     let mut fo = git2::FetchOptions::new();
-    fo.download_tags(git2::AutotagOption::All);
+    fo.prune(git2::FetchPrune::On);
     remote.fetch(refs, Some(&mut fo), None)?;
     if let Ok(fetch_head) = repo.find_reference("FETCH_HEAD") {
         return Ok(Some(repo.reference_to_annotated_commit(&fetch_head)?));
@@ -17,24 +17,10 @@ fn do_fetch<'a>(
     Ok(None)
 }
 
-fn do_reset<'a>(
-    repo: &'a git2::Repository,
-    fetch_commit: git2::AnnotatedCommit<'a>,
-) -> Result<(), git2::Error> {
-    let remote_obj = repo.find_commit(fetch_commit.id())?.into_object();
-    let mut checkout = git2::build::CheckoutBuilder::new();
-    checkout.force();
-    repo.reset(&remote_obj, git2::ResetType::Hard, None)?;
-    Ok(())
-}
-
-fn update_repo<P: AsRef<Path>>(path: P, repo: &Repo) -> Result<()> {
+fn update_repo<P: AsRef<Path>>(path: P) -> Result<()> {
     let r = git2::Repository::open(path.as_ref())?;
-    let remote_branch = &repo.default_branch;
     let mut remote = r.find_remote("origin")?;
-    if let Some(fetch_commit) = do_fetch(&r, &[remote_branch], &mut remote)? {
-        do_reset(&r, fetch_commit)?;
-    }
+    do_fetch(&r, &[], &mut remote)?;
 
     Ok(())
 }
@@ -42,9 +28,13 @@ fn update_repo<P: AsRef<Path>>(path: P, repo: &Repo) -> Result<()> {
 pub fn clone_or_update<P: AsRef<Path>>(path: P, repo: &Repo) -> Result<()> {
     let path = path.as_ref();
     if !path.exists() {
-        git2::Repository::clone(&repo.clone_url, path)?;
+        let mut builder = git2::build::RepoBuilder::new();
+        builder
+            .bare(true)
+            .remote_create(|repo, name, url| repo.remote_with_fetch(name, url, "+refs/*:refs/*"));
+        builder.clone(&repo.clone_url, path)?;
     } else {
-        update_repo(path, repo)?
+        update_repo(path)?
     }
 
     Ok(())
